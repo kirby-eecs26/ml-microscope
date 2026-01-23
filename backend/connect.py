5fc1f0b1869644bc0481508b32c3f1f700648012
Th3Gh0stK1ng <adrobny@uci.edu>
2026-01-22 21:21:51 -0800

    imports requests fro api connectivity, made functions apiConnect, disconnect, camConnect. Made error.py module for custom exception cases. testing not yet complete

--- backend/connect.py
+++ backend/connect.py
@@ -1,1 +1,51 @@
-print("hello world!")
+import requests
+import error
+
+#i put double spaces to distinguish between the functions
+
+API_BASE = "http://microscope.local:5000/" #we can either put the last "/" here or at
+                                            # hte beginning of the extensions, unsure which would be preferred
+
+def apiConnect(ip, timeout = 2.0):
+    """plan: take in the microscope ip for connection and connect to the API.
+    prints conformation if 200 returns raises; error if not"""
+    try:
+        response = requests.get(f"{API_BASE}api/v2", timeout=timeout)
+
+        if response.status_code == 200:
+            print("200 connection :)")
+        else:
+            raise error.APIConnectionError(response.status_code)
+
+    except error.APIConnectionError as e:
+        print(e.message)
+
+
+def camConnect(ip, timeout = 2.0):
+    """plan: take in the microscope ip for connection and conforms camera functionality.
+    prints conformation if (success code) returns; raises error if not"""
+    try:
+        response = requests.get(f"{API_BASE}api/v2/streams/mjpeg", timeout=timeout)
+
+        if response.status_code == 200: #change for correct camrea respons
+            print("camera connection :)")
+        else:
+            raise error.CameraConnectionError(response.status_code)
+
+    except error.CameraConnectionError as e:
+        print(e.message)
+
+
+def disconnect(ip):
+    """plan: take in the microscope ip for connection and disconnect to the API.
+    prints conformation if (success code) returns; raises error if not"""
+    try:
+        response = requests.get(f"{API_BASE}", timeout=2.0) #path extention for disconnecting from the api & microscope
+
+        if response.status_code == 200: #change for correct camrea respons
+            print("disconnected :)")
+        else:
+            raise error.APIDisconnectionError(response.status_code)
+
+    except error.APIDisconnectionError as e:
+        print("Could not disconnect :(" + e.message)
---
+++ backend/error.py
@@ -0,0 +1,30 @@
+#Custom exceptions
+
+class APIConnectionError(Exception):
+    def __init__(self, code):
+        self.message = "Microscope not found or could not connect"
+        self.error_code = code
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.message} (Error Code: {self.error_code})"
+
+
+class CameraConnectionError(Exception):
+    def __init__(self, code):
+        self.message = "Microscope Camera not found or malfunction"
+        self.error_code = code
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.message} (Error Code: {self.error_code})"
+
+
+class APIDisconnectionError(Exception):
+    def __init__(self, code):
+        self.message = "Microscope could not connect"
+        self.error_code = code
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.message} (Error Code: {self.error_code})"
