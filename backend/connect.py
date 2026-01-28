56cf6dfb6f753154a3849ad4d2ed667a40ab1f1a
Th3Gh0stK1ng <adrobny@uci.edu>
2026-01-27 23:24:09 -0800

    added mover and get position helper functions changed bound global variable in GlobalVariables.py but not edited in server.py found bounds and set stepsize for xy and z globals- thank you Roberto

--- backend/connect.py
+++ backend/connect.py
@@ -33,12 +33,27 @@ def camConnect(timeout = 2.0):
     prints conformation if (success code) returns; raises error if not"""
     try:
         response = requests.get(
-            f"{API_BASE}api/v2/streams/mjpeg",
-            timeout=timeout,
-            stream=True
-        )
+            f"{API_BASE}api/v2/streams/mjpeg", timeout=timeout)
 
         if response.status_code == 200: #change for correct camrea respons
+            print("camera connection :)")
+        else:
+            raise error.CameraConnectionError(response.status_code)
+
+    except error.CameraConnectionError as e:
+        print(e.message)
+
+    except requests.RequestException as e:
+        print("Microscope camera not found or malfunction")
+
+
+def restart(timeout = 2.0):
+    """plan: take in the microscope ip? for connection and conforms camera functionality.
+       prints conformation if (success code) returns; raises error if not"""
+    try:
+        response = requests.get(f"{API_BASE}", timeout=timeout)
+
+        if response.status_code == 200:  # change for correct camrea respons
             print("camera connection :)")
         else:
             raise error.CameraConnectionError(response.status_code)
@@ -54,7 +69,7 @@ def disconnect():
     """plan: take in the microscope ip for connection and disconnect to the API.
     prints conformation if (success code) returns; raises error if not"""
     try:
-        response = requests.get(f"{API_BASE}", timeout=2.0) #path extention for disconnecting from the api & microscope
+        response = requests.get(f"{API_BASE}api/v2/actions/system/shutdown/", timeout=2.0) #path extention for disconnecting from the api & microscope
 
         if response.status_code == 200: #change for correct camrea respons
             print("disconnected :)")
