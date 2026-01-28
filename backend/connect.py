55816fbaf5580485b5c7524936b2abfa264c7243
Roberto Lucatero <lucaterr@uci.edu>
2026-01-28 01:39:42 -0800

    started using client.py to talk with OFM, added return t/f for connect.py

--- backend/connect.py
+++ backend/connect.py
@@ -7,7 +7,7 @@ import json #?? precaution
 from libraries import GlobalVariables
 print("connect.py is running")
 
-API_BASE = "http://microscope.local:5000/" #we can either put the last "/" here or at        i think this is best
+API_BASE = "http://microscope.local:5000/" #we can either put the last "/" here or at
                                             # hte beginning of the extensions,
 
 def apiConnect(timeout = 2.0):
@@ -18,14 +18,17 @@ def apiConnect(timeout = 2.0):
 
         if response.status_code == 200:
             print("200 connection :)")
+            return True
         else:
             raise error.APIConnectionError(response.status_code)
 
     except error.APIConnectionError as e:
         print(e.message)
+        return False
 
     except requests.RequestException as e:
         print("Microscope not found or could not connect")
+        return False
 
 
 def camConnect(timeout = 2.0):
@@ -33,51 +36,39 @@ def camConnect(timeout = 2.0):
     prints conformation if (success code) returns; raises error if not"""
     try:
         response = requests.get(
-            f"{API_BASE}api/v2/streams/mjpeg", timeout=timeout)
+            f"{API_BASE}api/v2/streams/mjpeg", timeout=timeout, stream=True)
 
         if response.status_code == 200: #change for correct camrea respons
             print("camera connection :)")
+            return True
         else:
             raise error.CameraConnectionError(response.status_code)
 
     except error.CameraConnectionError as e:
         print(e.message)
+        return False
 
     except requests.RequestException as e:
         print("Microscope camera not found or malfunction")
-
-
-#def restart(timeout = 2.0):
- #   """plan: take in the microscope ip? for connection and conforms camera functionality.
-  #     prints conformation if (success code) returns; raises error if not"""
-   # try:
-    #    response = requests.get(f"{API_BASE}", timeout=timeout)
-#
- #       if response.status_code == 200:  # change for correct camrea respons
-  #          print("camera connection :)")
-   #     else:
-    #        raise error.CameraConnectionError(response.status_code)
-#
- #   except error.CameraConnectionError as e:
-  #      print(e.message)
-#
- #   except requests.RequestException as e:
-  #      print("Microscope camera not found or malfunction")
+        return False
 
 
 def disconnect():
     """plan: take in the microscope ip for connection and disconnect to the API.
     prints conformation if (success code) returns; raises error if not"""
     try:
-        response = requests.get(f"{API_BASE}api/v2/actions/system/shutdown/", timeout=2.0) #path extention for disconnecting from the api & microscope
+        response = requests.get(f"{API_BASE}api/v2/actions/system/shutdown/", timeout=2.0)
 
-        if response.status_code == 200: #change for correct camrea respons
+        if response.status_code == 200:
             print("disconnected :)")
+            return True
         else:
             raise error.APIDisconnectionError(response.status_code)
 
     except error.APIDisconnectionError as e:
         print("Could not disconnect :(" + e.message)
+        return False
+
 
 if __name__ == "__main__":
     apiConnect()
