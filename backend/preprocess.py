b1b368379b8ade172b20fb822120615be511ea94
Kirby <kirbylb@uci.edu>
2026-01-29 13:35:01 -0800

    Added 'cv' unit testing environment with sample images.

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -1,3 +1,7 @@
-#preprocess.py
-#takes in image and processes for other use
-#CV edit
+# preprocess.py
+#
+# Image preprocessing module.
+# Input:  (H x W x 3) uint8 RGB pixel data
+# Output: (H x W) float32 normalized grayscale pixel data
+
+
