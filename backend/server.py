cf6310e1232b377edebca22c95d6616c95414ff1
Th3Gh0stK1ng <adrobny@uci.edu>
2026-01-28 20:55:56 -0800

    remaded move made check cords function which also checks polar cords; getImg is updated with actual ppath and payload then a return fro the latest img dict; still need to fix getVideo outlined future functionality functions for later if needed idk i dont think we did anything else

--- backend/server.py
+++ backend/server.py
@@ -5,148 +5,96 @@
         #extentions/values/proper returns are fleshed out until then DO NOT RUN WILL NOT COMPILE
 
 import time
+import math
+from ctypes.wintypes import tagMSG
+
 import numpy as np
+import json
 import requests
 from libraries.GlobalVariables import (API_BASE, XY_STEPSIZE, Z_STEPSIZE, NEG_X_BOUND,
                                        POS_X_BOUND, POS_Y_BOUND, NEG_Y_BOUND, POS_Z_BOUND, NEG_Z_BOUND)
 
 #Helper Functions
-def getCurrentPostion():
-    print("getCurrentPostion")
-    try:
-        response = requests.get(f"{API_BASE}", )
-        return response.json()
 
-    except Exception as e:
-        print("Error getting current postion")
-
-
-# def x():
-#     return getCurrentPostion()["x,y,z"] #return x cord
-#
-#
-# def y():
-#     return getCurrentPostion()["x,y,z"] #return y cord
-#
-#
-# def z():
-#     return getCurrentPostion()["x,y,z"] #return y cord
-#
-#
-# #actual button functions to hw api
-# def moveNegX():
-#     """plan: get grid; if: point + step = out of bounds do nothing
-#        (raise out of bounds error?? at least for test and logging);
-#        else: try api move; raise error if issue"""
-#     print("moveleft")
+#fro arrow keys
+# def getCurrentPostion(timeout = 2.0):
+#     print("getCurrentPostion")
 #     try:
-#         if ((STEPSIZE + x()) < LEFTBOUND):
-#             raise Exception("out of bounds")
+#         response = requests.get(f"{API_BASE}api/v2/instrument/state/stage/position", timeout=timeout)
+#         return response.json()
 #
-#         response = requests.get(f"{API_BASE}(extention)", timeout=2.0)
-#         # return code? json?
-#
-#     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, Exception) as e:
-#         print("Movement error: motor could not move or movement out of bounds")
-#
-#
-# def movePosX():
-#     """plan: get grid; if: point + step = out of bounds do nothing
-#            (raise out of bounds error?? at least for test and logging);
-#            else: try api move; raise error if issue"""
-#     print("moveright")
-#     try:
-#         if ((STEPSIZE + x()) > RIGHTBOUND):
-#             raise Exception("out of bounds")
-#
-#         response = requests.get(f"{API_BASE}(extention)", timeout=2.0)
-#         # return code? json?
-#
-#     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, Exception) as e:
-#         print("Movement error: motor could not move or movement out of bounds")
-#
-#
-# def moveNegY():
-#     """plan: get grid; if: point + step = out of bounds do nothing
-#            (raise out of bounds error?? at least for test and logging);
-#            else: try api move; raise error if issue"""
-#     print("movedown")
-#     try:
-#         if ((STEPSIZE + y()) < BOTTOMBOUND):
-#             raise Exception("out of bounds")
-#
-#         response = requests.get(f"{API_BASE}(extention)", timeout=2.0)
-#         # return code? json?
-#
-#     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, Exception) as e:
-#         print("Movement error: motor could not move or movement out of bounds")
-#
-#
-# def movePosY():
-#     """plan: get grid; if: point + step = out of bounds do nothing
-#            (raise out of bounds error?? at least for test and logging);
-#            else: try api move; raise error if issue"""
-#     print("moveup")
-#     try:
-#         if((STEPSIZE + y()) > TOPBOUND):
-#             raise Exception("out of bounds")
-#
-#         response = requests.get(f"{API_BASE}(extention)", timeout=2.0)
-#         #return code? json?
-#
-#     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, Exception) as e:
-#         print("Movement error: motor could not move or movement out of bounds")
-#
-#
-# #FORZ: need to know neg vs pos z cords actually interface with movement come back to these
-# def movePosZ():
-#     """plan: get grid; if: point + step = out of bounds do nothing
-#        (raise out of bounds error?? at least for test and logging);
-#        else: try api move; raise error if issue"""
-#     print("moveleft")
-#     try:
-#         if ((STEPSIZE + x()) < LEFTBOUND):
-#             raise Exception("out of bounds")
-#
-#         response = requests.get(f"{API_BASE}(extention)", timeout=2.0)
-#         # return code? json?
-#
-#     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, Exception) as e:
-#         print("Movement error: motor could not move or movement out of bounds")
-#
-# def moveNegZ():
-#     """plan: get grid; if: point + step = out of bounds do nothing
-#        (raise out of bounds error?? at least for test and logging);
-#        else: try api move; raise error if issue"""
-#     print("moveleft")
-#     try:
-#         if ((STEPSIZE + x()) < LEFTBOUND):
-#             raise Exception("out of bounds")
-#
-#         response = requests.get(f"{API_BASE}(extention)", timeout=2.0)
-#         # return code? json?
-#
-#     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, Exception) as e:
-#         print("Movement error: motor could not move or movement out of bounds")
+#     except Exception as e:
+#         print("Error getting current postion")
 
 
-def move():
+def checkCords(x, y, z) -> bool:
+    print("checkCords")
+
+    d = math.sqrt(x**2 + y**2)
+    if (d <= POS_X_BOUND):
+        return True
+    elif(NEG_X_BOUND <= x <= POS_X_BOUND):
+        return True
+    elif(NEG_Y_BOUND <= y <= POS_Y_BOUND):
+        return True
+    elif(NEG_Z_BOUND >= z >= POS_Z_BOUND):
+        return True
+    else:
+        return False
+
+
+# def moveArrows():
+
+
+def moveButton(x: int, y: int, z: int, timeout = 2.0):
     print("move")
     try:
-        switch(getCurrentPostion()):
+        if(checkCords(x, y, z)):
+            payload = {
+                "x": x,
+                "y": y,
+                "z": z,
+                "absolute": True
+            }
+
+            print("Cords moved")
+            response = requests.post(f"{API_BASE}api/v2/actions/stage/move", json=payload, timeout=timeout)
+            return response.json()
+
+        else:
+            print("Cords not moved")
+            raise Exception("Cords not moved")
+
+    except Exception as e:
+        print("Cords not moved")
 
 
-        response = requests.get(f"{API_BASE}", timeout=2.0)
-
-
-def getImg(): #-> jpeg?
+def getImg(payload: dict, timeout = 2.0): #-> jpeg?
     print("getImg")
     try:
-       response = requests.get(f"{API_BASE}(extention)", timeout=2.0)
-       return response.json() #don't want to actually return json needs help for actual img return val
+        response = requests.post(f"{API_BASE}api/v2/actions/camera/capture", json=payload, timeout=timeout)
+        response.raise_for_status()
+
+        captureList = requests.get(f"{API_BASE}api/v2/actions/camera/get", timeout=timeout)
+        captureList.raise_for_status()
+        allcaptures = captureList.json()
+
+        return allcaptures[-1] #returns last taken picture
 
     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
         print("Camera error: could not take img")
+
+
+def listCaptures(timeout = 2.0):
+    print("listCaptures")
+    try:
+        response = requests.get(f"{API_BASE}api/v2/captures", timeout=timeout)
+        response.raise_for_status()
+        return response.json()
+
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        print("Camera error: could not list captures")
+
 
 def mjpeg_stream_url():
     return f"{API_BASE}api/v2/streams/mjpeg"
