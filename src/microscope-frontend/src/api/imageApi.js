c4b5837ebc813e9546368552f017da1c69e2cbad
Roberto Lucatero <lucaterr@uci.edu>
2026-02-04 23:09:28 -0800

    Working View and Movement Button

--- src/microscope-frontend/src/api/imageApi.js
+++ src/microscope-frontend/src/api/imageApi.js
@@ -3,7 +3,7 @@
 const BASE_URL = "http://127.0.0.1:8000"; // backend port
 
 export async function captureImage(payload) {
-  const res = await fetch(`${BASE_URL}/api/capture/image`, {
+  const res = await fetch(`${BASE_URL}/capture`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
@@ -15,4 +15,19 @@ export async function captureImage(payload) {
   }
 
   return await res.json();
+}
+
+export async function moveAbs(x, y, z) {
+  const res = await fetch(`${BASE_URL}/move`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      x: Math.round(Number(x)),
+      y: Math.round(Number(y)),
+      z: Math.round(Number(z)),
+    }),
+  });
+
+  if (!res.ok) throw new Error(await res.text());
+  return res.json();
 }
