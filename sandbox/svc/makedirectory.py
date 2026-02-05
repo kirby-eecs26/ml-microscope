e0f9f7014030731383a18f779d65c14f5a48f54c
Th3Gh0stK1ng <adrobny@uci.edu>
2026-02-04 19:28:12 -0800

    finished video?? tho needs testing, working on downloads

--- sandbox/svc/makedirectory.py
+++ sandbox/svc/makedirectory.py
@@ -3,18 +3,22 @@ import os
 from contextlib import chdir
 
 # path = Path.cwd()
-# path.mkdir(parents=True, exist_ok=True)
-# path.rename(Path.cwd() / f"Images")
-
-#os.mkdir("Images")
+# # path.mkdir(parents=True, exist_ok=True)
+# # path.rename(Path.cwd() / f"Images")
+#
+# os.mkdir("Images")
+# print(f"Created local path: {path}")
+#
+#
+# path = Path.cwd()
+# imgpath = path / "Images"
+# print(imgpath)
+#
+# with chdir(imgpath):
+#     print(f"Created local path: {path}")
+#
 # print(f"Created local path: {path}")
 
+downloads_folder = os.path.join(os.getenv("USERPROFILE"), "Downloads")
 
-path = Path.cwd()
-imgpath = path / "Images"
-print(imgpath)
-
-with chdir(imgpath):
-    print(f"Created local path: {path}")
-
-print(f"Created local path: {path}")
+file_path = os.path.join(downloads_folder, "placeholder.txt")
