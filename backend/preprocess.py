bf32f51aec76fbbede12ac6733075276e9260fd6
Kirby <kirbylb@uci.edu>
2026-02-05 18:44:29 -0800

    preprocess v2.1

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -5,7 +5,7 @@
 # Converts input tensor from color JPEG to normalized grayscale tensor.
 #
 # Input:  (H x W x 3) uint8 RGB pixel data
-# Output: (H x W) float32 normalized grayscale pixel data
+# Output: (H x W) float32 normalized grayscale pixel data 
 # ======================================================================
 
 import numpy as np
