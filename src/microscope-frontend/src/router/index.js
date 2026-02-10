a2dba694d3ee7db764237a795562224276d82845
Dylan Le <dble2@uci.edu>
2026-02-10 00:28:22 -0800

    video.vue added

--- src/microscope-frontend/src/router/index.js
+++ src/microscope-frontend/src/router/index.js
@@ -3,9 +3,22 @@ import View from "../window/View.vue";
 import Gallery from "../window/Gallery.vue";
 import Move from "../window/Move.vue";
 import Image from "../window/Image.vue";
+<<<<<<< Updated upstream
 import About from "../window/About.vue";
 import Logging from "../window/Logging.vue";
 import Settings from "../window/Settings.vue";
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+import About from "../window/About.vue";
+import Logging from "../window/Logging.vue";
+import Settings from "../window/Settings.vue";
+=======
+import Video from "../window/Video.vue";
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 export default createRouter({
   history: createWebHistory(),
@@ -15,8 +28,21 @@ export default createRouter({
     { path: "/gallery", component: Gallery },
     { path: "/move", component: Move },
     { path: "/image", component: Image },
+<<<<<<< Updated upstream
     { path: "/about", component: About},
     {path: "/logging", component: Logging},
     {path: "/settings", component: Settings},
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+    { path: "/about", component: About},
+    {path: "/logging", component: Logging},
+    {path: "/settings", component: Settings},
+=======
+    { path: "/video", component: Video },
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   ],
 });
