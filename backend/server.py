bdf4337a8392407a22205fcce263c7b863add6c9
Addison Drobny <adrobny@uci.edu>
2026-02-13 12:21:35 -0800

    docstrings and made a todo list for some of the tweaks needing to happen in all backend files, found some errors in server and save that need fixing that i couldnt get to. added the save ml analysis function - yet to be tested focusing on saving image.

--- backend/server.py
+++ backend/server.py
@@ -18,26 +18,36 @@ from libraries.GlobalVariables import (API_BASE, POS_X_BOUND, POS_Z_BOUND, NEG_Z
 #Helper Functions
 
 #for arrow keys
-# def getCurrentPosition(timeout = 2.0):
-#     print("getCurrentPosition")
-#     try:
-#         response = (requests.get(f"{API_BASE}api/v2/instrument/state/stage/position", timeout=timeout)).json()
-#         return (response.x, response.y, response.z) #return x y z tuple
-#
-#     except Exception as e:
-#         print("Error getting current position")
+
 
 
 def apiHealth(timeout=2.0) -> bool:
+    """
+    requests api health diagnostics that checks that motors or in order and that the
+    api has stable connection, raises error if connection and/or microscope unstable
+    todo: add error to log
+    :param timeout:
+    :return: bool of good api health or not
+    """
     try:
         response = requests.get(f"{API_BASE}api/v2", timeout=timeout)
         response.raise_for_status()
+
         return response.status_code == 200
     except requests.RequestException:
         return False
 
 
 def checkCords(x, y, z) -> bool:
+    """
+    takes in x,,yz coordinates and checks that the points are in the circular
+    bounds of the microscope case and stops before it hits the slide or
+    z gets to low in bounds
+    :param x: x axis cord
+    :param y: y axis cord
+    :param z: z axis cord (-z is closer to microscope, +z is further from microscope)
+    :return: bool of new coordinates are in bounds or not
+    """
     print("checkCords")
 
     d = math.sqrt(x**2 + y**2)
@@ -49,8 +59,31 @@ def checkCords(x, y, z) -> bool:
     else:
         return False
 
+def get_position(timeout=8.0) -> dict:
+    """
+    Returns current stage position from OpenFlexure. raises error if api call wrong then catches
+    todo: add exceptions to logging
+    :param timeout:
+    :return: dicts of the xyz coordinate positions
+    """
+    r = requests.get(f"{API_BASE}api/v2/instrument/state/stage/position", timeout=timeout)
+    r.raise_for_status()
+    pos = r.json()
+    pos["x"] = -pos["x"]
+    return pos
+
+# def check_step(cords: dict):
+# """plan: directions moving add it to temp cord check in or out of bounds return true if in bounds. false if out of bounds"""
+#     if
+
 
 # def moveArrows():
+#     """
+#     moving postion with arrow keys on step at a time
+#     checks step will not be out of bounds the sends movement call to teh api for execution
+#     if error raise and catch
+#     todo: logging of errors
+#     """
 #     print("moveArrows")
 #     try:
 #         if :
@@ -63,15 +96,18 @@ def checkCords(x, y, z) -> bool:
 #
 #
 
-def get_position(timeout=8.0) -> dict:
-    """Returns current stage position from OpenFlexure."""
-    r = requests.get(f"{API_BASE}api/v2/instrument/state/stage/position", timeout=timeout)
-    r.raise_for_status()
-    pos = r.json()
-    pos["x"] = -pos["x"]
-    return pos
-
 def moveButton(x: int, y: int, z: int, timeout = 60.0):
+    """
+    check new move coordinates are in bounds then request api to move lense and camera to
+    new position, if coords not in bounds raise exception, if response status is an error raise
+    exception, catch exeptions and put in log
+    todo: add exceptions to logging
+    :param x:
+    :param y:
+    :param z:
+    :param timeout:
+    :return: json response
+    """
     x = -x
     print("moveButton")
     try:
@@ -96,8 +132,14 @@ def moveButton(x: int, y: int, z: int, timeout = 60.0):
         print(e.__str__())
 
 
-def captureImg(payload: dict, timeout = 2.0): #-> jpeg?
-    """take image and save to local pi"""
+def captureImg(payload: dict, timeout = 2.0):
+    """
+    take image and save to local pi
+    return latest image dict
+    :param payload:
+    :param timeout:
+    :return: image dict
+    """
     print("captureImg")
     try:
         response = requests.post(f"{API_BASE}api/v2/actions/camera/capture", json=payload, timeout=timeout)
@@ -115,7 +157,11 @@ def captureImg(payload: dict, timeout = 2.0): #-> jpeg?
 
 
 def listCaptures(timeout = 2.0):
-    """returns a list of all saved images on the local pi"""
+    """
+    returns a list of all saved image dicts on the local pi
+    :param timeout:
+    :return: list of dicts
+    """
     print("listCaptures")
     try:
         response = requests.get(f"{API_BASE}api/v2/actions/camera/get", timeout=timeout)
@@ -134,6 +180,16 @@ def mjpeg_stream_url():
 
 
 def captureVideo(fpm: int, payload: dict, duration: float = MAX_DURATION_SEC) -> list:
+    """
+    takes in pyload of image capture annotations and notes, fpm, and duration,
+    takes an image every few seconds to fit in the frames per min time arg,
+    default duration of 15 seconds.
+    errors are handeled in the image capture function
+    :param fpm:
+    :param payload:
+    :param duration: default 15 seconds
+    :return: list of image dicts w/ annotations and image access links
+    """
     print("getVideo")
     spf: float = 60 / fpm
 
@@ -141,7 +197,7 @@ def captureVideo(fpm: int, payload: dict, duration: float = MAX_DURATION_SEC) ->
     start_time = time.time()
     i = 0
 
-    while time.time() - start_time < duration:
+    while time.time() - start_time < min(duration, MAX_DURATION_SEC):
         video.append(captureImg(payload))
         # video.append(f"img{i}")
         # i += 1
